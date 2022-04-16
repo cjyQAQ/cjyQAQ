5faf6e0856383abae33ca0ac5430ed7dff2f6bcb
cjyQAQ <1103755240@qq.com>
2022-04-16 19:40:25 +0800

    首次提交cperson文件

---
+++ cperson.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Cperson
+{
+   public:
+	   Cperson();
+	   ~Cperson();
+   public:
+	  static int a;
+	  static int b;
+}
+
+int main()
+{
+	Cpersson CP = new Cperson();
+	return 0;
+}
