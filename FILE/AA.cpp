84f079190de207c5dd07a4d20b3c9572d68c67b8
cjyQAQ <1103755240@qq.com>
2022-04-16 16:20:37 +0800

    提交

---
+++ FILE/AA.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class AA
+{
+public:		
+	AA();
+	~AA();
+private:
+	static int s;
+	static int t;
+};
+int main()
+{
+	AA a = new AA();
+	a.t = 10;
+	a.s = 20;
+	cout<<a.t<<endl;
+	cout<<a.s<<endl;
+	return 0;
+}
+
