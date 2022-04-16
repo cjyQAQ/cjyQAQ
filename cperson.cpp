fb2a0c7f7f19fbe4ae644c16f07887d4091c0a1b
cjyQAQ <1103755240@qq.com>
2022-04-16 19:59:23 +0800

    cperson修改后提交

--- cperson.cpp
+++ cperson.cpp
@@ -6,11 +6,15 @@ class Cperson
    public:
 	   Cperson();
 	   ~Cperson();
+   private:
+	   static Cperson *ps;
+	   static pthread_mutex_t lock;
    public:
 	  static int a;
 	  static int b;
 }
 
+Cperson *p1 = NULL;
 int main()
 {
 	Cpersson CP = new Cperson();
